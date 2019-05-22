15ec9d22850c050824c518d5c0ce2dde0a7c8f32
Dustin Haefele <Dustin.haefele@gmail.com>
2019-05-22 15:25:44 -0400

    functional.  Need tests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import RouletteGun from './state-drills/RouletteGun';
+import Accordian from './state-drills/Accordian';
 import './App.css';
 
 function App() {
+  const sections = [
+    {
+      title: 'Section 1',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      title: 'Section 2',
+      content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+    },
+    {
+      title: 'Section 3',
+      content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+    },
+  ]
   return (
     <div className="App">
-      <RouletteGun bulletInChamber={4} />
+      <Accordian sections={sections} />
     </div>
   );
 }
---
+++ src/state-drills/Accordian.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class Accordian extends React.Component{
+  static defaultProps = {sections: []}
+
+  state = {
+    currentSectionIndex: null,
+  }
+
+  handleClick = (index) => {
+    this.setState({
+      currentSectionIndex: index,
+    })
+  }
+
+ 
+  
+  renderSections(item, index){
+   
+      return (
+      <li key={index}>
+        <button  onClick={() => this.handleClick(index)}>
+        {item.title}
+        </button>
+        {(this.state.currentSectionIndex === index) && <p>{item.content}</p>}
+      </li>)
+    }
+  
+
+  render(){
+
+    return (
+    <ul>
+      {this.props.sections.map((item,index) => 
+        this.renderSections(item, index)
+      )}
+    </ul>)
+  }
+}
+
+export default Accordian;
